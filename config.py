2db096797b35bd3f0532e074df87e66e83979a17
ndanu-josy <josphinendanu3@gmail.com>
2021-06-11 22:00:39 +0300

    Update config file

---
+++ config.py
@@ -0,0 +1,52 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+  
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:josie@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+   
+   # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    pass
+class ProdConfig(Config):
+    '''
+    Production configuration 
+
+    Args:
+    Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args: 
+    Config The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:josie@localhost/watchlist'
+    DEBUG = True
+
+class TestConfig(Config):
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:josie@localhost/watchlist'
+
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
+  
