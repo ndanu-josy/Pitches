84b14ac79c7cca1c3be7c60db9c4b3ea07dd72b7
ndanu-josy <josphinendanu3@gmail.com>
2021-06-12 11:08:28 +0300

    Add view functions for the classes

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,21 +2,21 @@ from wtforms.validators import Required
 from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from flask_wtf import FlaskForm
 
-class PitchesForm(FlaskForm):
+class PitchForm(FlaskForm):
     """
     Class to create a wtf form for creating a pitch
     """
     content = TextAreaField('Write a pitch')
     submit = SubmitField('Submit')
 
-class CommentsForm(FlaskForm):
+class CommentForm(FlaskForm):
     """
     Class to create a wtf form for adding comments
     """
     opinion = TextAreaField('Add a comment')
     submit = SubmitField('Submit')
 
-class CategoriesForm(FlaskForm):
+class CategoryForm(FlaskForm):
     """
     Class to create a wtf form for adding pitch categories
     """
