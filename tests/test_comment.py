e3c66e2e9e0e6bac45479bd063aa0eeb913c9d4d
ndanu-josy <josphinendanu3@gmail.com>
2021-06-13 23:10:17 +0300

    comment test

---
+++ tests/test_comment.py
@@ -0,0 +1,26 @@
+import unittest
+from app.models import Comments
+
+
+class CommentModelTest(unittest.TestCase):
+    """
+    Test Class to test the behaviour of the Comment class
+    """
+
+    def setUp(self):
+        """
+        Set up method that will run before every Test
+        """
+        self.comment= Comments(opinion = 'I wanna try everything')
+
+
+    def tearDown(self):
+        Comments.query.delete()
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comment, Comments))
+
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.comment.opinion,'I wanna try everything')
